84e3d00a676e0c95f3a6404107441f55762be9c3
AARONJVC <avanclea@cs.uoregon.edu>
2021-10-03 20:38:08 -0400

    Beginning of the setup script created

--- auto_journal_setup.sh
+++ auto_journal_setup.sh
@@ -1,3 +1,38 @@
 #!/bin/bash
 
-echo "This script will be run once to set up the directory structure and files that are otherwise hidden via gitignore";
+config_dir="Configuration";
+
+# Make the Configuration folder
+if [[ ! -d "$config_dir" ]];
+then
+    mkdir "$config_dir";
+fi
+
+archive_script_name="set_archive_target.sh";
+
+# Place the Archive configuration script in the Configuration folder
+if [[ ! -f "${config_dir}\\${archive_script_name}" ]];
+then
+    
+    printf "Enter an absolute filepath where you'd like journal entries to be stored:\n";
+
+    read -r archive_target;
+    
+    archive_script_text="#!/bin/bash
+
+archive_target=\"${archive_target}\";
+
+echo \"\$archive_target\";";
+
+    echo "$archive_script_text" >> "${config_dir}\\${archive_script_name}";
+fi
+
+logging_dir="Logs";
+logfile_name="auto_journal_log.txt";
+
+# Make the Logs folder
+if [[ ! -d "$logging_dir" ]];
+then
+    mkdir "$logging_dir";
+    touch "${logging_dir}\\${logfile_name}";
+fi
